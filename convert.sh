80fb8cff1fccf52b3b856ceeecf9bb9de3e0e361
bobchenyx <chen.yixia@northeastern.edu>
2026-05-16 15:46:52 -0400

    update convert.sh with local scripts

--- convert.sh
+++ convert.sh
@@ -6,3 +6,8 @@
 #     --push_to_hub
 
 #  Unitree_G1_Dex1 / Unitree_G1_Dex1_sim
+
+# python unitree_lerobot/utils/convert_unitree_json_to_lerobot_local.py \
+#     --raw-dir <> \
+#     --robot_type Unitree_G1_Dex1_Sim \
+#     --root <>
